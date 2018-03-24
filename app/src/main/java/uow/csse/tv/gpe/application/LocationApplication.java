f1ec03433df99e6100cacc5ccb97f12a8d218484
Tab Tu <tabtu@ttxy.org>
2018-03-24 10:17:52 +0800

    add bdmap api support

---
+++ app/src/main/java/uow/csse/tv/gpe/application/LocationApplication.java
@@ -0,0 +1,29 @@
+package uow.csse.tv.gpe.application;
+
+import android.app.Application;
+import android.app.Service;
+import android.os.Vibrator;
+
+import uow.csse.tv.gpe.service.LocationService;
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * Created by Tab on 2018-03-24.
+ */
+
+public class LocationApplication extends Application {
+    public LocationService locationService;
+    public Vibrator mVibrator;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /***
+         * 初始化定位sdk，建议在Application中创建
+         */
+        locationService = new LocationService(getApplicationContext());
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+        SDKInitializer.initialize(getApplicationContext());
+//        SDKInitializer.initialize(getActivity().getApplicationContext());
+
+    }
+}
